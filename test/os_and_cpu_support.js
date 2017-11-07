26070e895e8db7817af9f3cabd67d108446492cb
David Gaya <david.gaya@starbreeze.com>
2017-11-07 18:25:35 +0100

    clone request stream to workaround Promise incompatibility

--- test/os_and_cpu_support.js
+++ test/os_and_cpu_support.js
@@ -49,11 +49,12 @@ describe('os and cpu options', function () {
     // install should do nothing (maybe only info message) if it is an ignored platform.
     describe("install", function () {
         const patch_version = require('crypto').randomBytes(4).readUInt32LE();
+        const packageJson = std_package_json(patch_version);
 
         beforeEach( clean_install_dir );
 
         before(() =>
-            writePublishPackageJson(std_package_json(patch_version))
+            writePublishPackageJson(packageJson)
               .then(npm_unpublish)
               .then(publish)
         );
@@ -61,25 +62,23 @@ describe('os and cpu options', function () {
         after(() => unpublish());
 
         it('installs if there is no os or cpu', () =>
-            writeInstallPackageJson(std_package_json(patch_version)).then(install)
+            writeInstallPackageJson(packageJson).then(install)
                 .then(() => assert_exists("./test/install/boost"))
         );
         it('installs if the os and cpu are enabled', function () {
-            const packageJson = std_package_json(patch_version);
+
             packageJson.binary.os = [process.platform];
             packageJson.binary.cpu = [process.arch];
             return writeInstallPackageJson(packageJson).then(install)
                 .then(() => assert_exists("./test/install/boost"));
         });
         it('skips install if the os in NOT enabled', function () {
-            const packageJson = std_package_json(patch_version);
             packageJson.binary.os = ["martian"];
             packageJson.binary.cpu = [process.arch];
             return writeInstallPackageJson(packageJson).then(install)
                 .then(() => assert_not_exists("./test/install/boost"));
         });
         it('skips install if the cpu in NOT enabled', function () {
-            const packageJson = std_package_json(patch_version);
             packageJson.binary.os = [process.platform];
             packageJson.binary.cpu = ["martian"];
             return writeInstallPackageJson(packageJson).then(install)
